66f80aa342995c04ded40f58e29816944cd824be
SHABIE IQBAL <shabie.iqbal@daimler.com>
2019-11-21 12:58:44 +0100

    Correct indents

--- access.py
+++ access.py
@@ -5,10 +5,11 @@ client_id = 'YOUT_CLIENT_ID'
 client_secret = 'YOUR_CLIENT_SECRET'
 resource = 'https://datalake.azure.net/'
 
-adls_creds = lib.auth(tenant_id=tenant_id, 
-				      client_id=client_id, 
-				      client_secret=client_secret, 
-				      resource=resource)
+adls_creds = lib.auth(tenant_id=tenant_id,
+		      client_id=client_id,
+		      client_secret=client_secret,
+		      resource=resource,
+		     )
 				  
 				  
 adlsfs = core.AzureDLFileSystem(adls_creds, 'YOUR_ADLS_NAME')
